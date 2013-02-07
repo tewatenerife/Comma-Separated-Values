a2f3ffc563e776045161ce9c1b101d0f14cc5745
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-02-07 21:01:19 +0000

    working with underscore

--- csv.js
+++ csv.js
@@ -7,11 +7,12 @@ function calculate() {
   var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
   var lines = temp.split(/\n+\s*/);
   var r = [];
+  var row = "<% _.each(items, function(name) { %> <td><%= name %></td> <% }); %>";
 
   for(var t in lines) {
-    temp = lines[t];
+    var temp = lines[t];
     var m = temp.match(regexp);
-    var result = '';
+    var result = [];
     
     if (m) {
       for(var i in m) {
@@ -19,13 +20,13 @@ function calculate() {
         var remove1stquote = removecomma.replace(/^\s*"/,'');
         var removelastquote = remove1stquote.replace(/"\s*$/,'');
         var removeescapedquotes = removelastquote.replace(/\\"/,'"');
-        result += "<td>"+removeescapedquotes+"</td> ";
+        result.push(removeescapedquotes);
       }
-      result = "<tr>"+result+"</tr>";
-      r.push(result);
+      alert(result);
+      r.push("<tr>"+_.template(row, {items : result})+"</tr>");
     }
     else {
-      r.push('ERROR! at '+i);
+      alert('ERROR! at row '+i);
     }
   }
   r.unshift('<p>\n<table id="result">');
