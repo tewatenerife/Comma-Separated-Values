0fb495baff68dcbd2768c1009bb374dcff6ecbb6
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-02-07 19:13:16 +0000

    Now conforms a bit more to wikipedia description of CSV

--- csv.js
+++ csv.js
@@ -1,23 +1,35 @@
+// See http://en.wikipedia.org/wiki/Comma-separated_values
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 function calculate() {
   var result;
   var original       = document.getElementById("original");
   var temp = original.value;
   var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
-  var m = temp.match(regexp);
-  var result = '';
-  
-  if (m) {
-    for(var i in m) {
-      var removecomma = m[i].replace(/,\s*$/,'');
-      var remove1stquote = removecomma.replace(/^\s*"/,'');
-      var removelastquote = remove1stquote.replace(/"\s*$/,'');
-      result += "<li>"+removelastquote+"\n";
+  var lines = temp.split(/\n+\s*/);
+  var r = [];
+
+  for(var t in lines) {
+    temp = lines[t];
+    var m = temp.match(regexp);
+    var result = '';
+    
+    if (m) {
+      for(var i in m) {
+        var removecomma = m[i].replace(/,\s*$/,'');
+        var remove1stquote = removecomma.replace(/^\s*"/,'');
+        var removelastquote = remove1stquote.replace(/"\s*$/,'');
+        var removeescapedquotes = removelastquote.replace(/\\"/,'"');
+        result += "<td>"+removeescapedquotes+"</td> ";
+      }
+      result = "<tr>"+result+"</tr>";
+      r.push(result);
     }
-    result = "<ol>\n"+result+"</ol>";
-    converted.innerHTML = result;
+    else {
+      r.push('ERROR! at '+i);
+    }
   }
-  else {
-    converted.innerHTML = 'ERROR! not a CSV string. Try "earth",1, "a1, a2", "moon",9.374,instead';
-  }
+  r.unshift('<p>\n<table id="result">');
+  r.push('</table>');
+  alert(r.join('\n'));
+  converted.innerHTML = r.join('\n');
 }
