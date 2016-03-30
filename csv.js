87354044760e82f84507621c8e6ba7670eb0827f
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2016-03-30 23:41:52 +0100

    factorizing

--- csv.js
+++ csv.js
@@ -5,8 +5,15 @@
   var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
   exports.calculate = function(original) {
     var lines = original.split(/\n+\s*/);
-    var commonLength = NaN;
+    var commonLength = lines[0].match(regexp).length;
     var r = [];
+    var removeQuotes = function(field) {
+      var removecomma = field.replace(/,\s*$/, '');
+      var remove1stquote = removecomma.replace(/^\s*"/, '');
+      var removelastquote = remove1stquote.replace(/"\s*$/, '');
+      var removeescapedquotes = removelastquote.replace(/\\"/, '"');
+      return removeescapedquotes;
+    };
 
     for (var t in lines) {
       var temp = lines[t];
@@ -15,19 +22,11 @@
       var error = false;
 
       if (m) {
-        if (commonLength && (commonLength != m.length)) {
+        if (commonLength != m.length) {
           error = true;
         }
-        else {
-          commonLength = m.length;
-          error = false;
-        }
         for (var i in m) {
-          var removecomma = m[i].replace(/,\s*$/, '');
-          var remove1stquote = removecomma.replace(/^\s*"/, '');
-          var removelastquote = remove1stquote.replace(/"\s*$/, '');
-          var removeescapedquotes = removelastquote.replace(/\\"/, '"');
-          result.push(removeescapedquotes);
+          result.push(removeQuotes(m[i]));
         }
         var rowclass = error? 'error' : '';
         r.push({ value: result, rowClass: rowclass });
