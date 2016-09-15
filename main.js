6617e61cbdcdf8658919f545ec7ff7a694ef5afd
tewatenerife <teguayco95@gmail.com>
2016-09-15 18:24:42 +0100

    drag n' drop added

--- main.js
+++ main.js
@@ -14,6 +14,28 @@ function dump(fileName) {
 	});
 }
 
+function handleFileSelect(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+
+	var files = evt.dataTransfer.files;	// FileList object
+	var reader = new FileReader();
+
+	reader.onload = function(e) {
+		var content = e.target.result;
+		$("#original").val(content);
+	};
+
+	// files is a FileList of File objects. List some properties.
+	// We'll just read the first File object.
+	reader.readAsText(files[0]);
+}
+
+function handleDragOver(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+}
+
 $(document).ready(function() {
 	// If the browser supports localStorage and we have some stored data
 	if (window.localStorage && localStorage.original)
@@ -35,7 +57,11 @@ $(document).ready(function() {
 
 	$(".examplesbuttons button").click( function(event) {
 		var id = this.id;
-		console.log("you clicked the button " + id);
 		dump("examples/" + id + ".txt");
 	});
+
+	// Setup the drag and drop listeners.
+	var dropZone = document.getElementById('original');
+	dropZone.addEventListener('dragover', handleDragOver, false);
+	dropZone.addEventListener('drop', handleFileSelect, false);
 });
